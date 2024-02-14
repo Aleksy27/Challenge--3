7f8288d1b3a793d1b9c67434a807633e973b9232
Aleksy27 <aleksanderpiotrowski194@gmail.com>
2024-02-14 23:10:37 +0100

    changes

--- main.py
+++ main.py
@@ -1,38 +1,33 @@
 import sqlite3
 import hashlib
-import sys
 
-sys.stdout.reconfigure(encoding='utf-8')
-
-def database():
+def process_database():
     conn = sqlite3.connect(r'users-challenge.db')
     cursor = conn.cursor()
     cursor.execute("SELECT email, password FROM users")
     data = cursor.fetchall()
     conn.close()
-    return data
 
-def decryption(encrypted_password):
     hashes_list = ['sha1', 'sha224', 'sha256', 'sha384', 'sha512', 'md5']
 
     with open(r'worldlist.txt', 'r', encoding='utf-8') as file:
-        for row in file:
-            password = row.strip()
-            for i in hashes_list:
-                decrypt = hashlib.new(i, password.encode()).hexdigest()
-                if decrypt == encrypted_password:
-                    return password
-    return None
+        for email, encrypted_password in data:
+            password_found = False
+            for row in file:
+                password = row.strip()
+                for i in hashes_list:
+                    decrypt = hashlib.new(i, password.encode()).hexdigest()
+                    if decrypt == encrypted_password:
+                        print(f"{email}: {password} ok")
+                        password_found = True
+                        break
+                if password_found:
+                    break
+            else:
+                print(f"Could not decrypt password for {email}")
 
 def main():
-    db = database()
-
-    for email, encrypted_password in db:
-        decrypt_password =  decryption(encrypted_password)
-        if decrypt_password:
-            print(f"{email}: {decrypt_password} ok")
-        else:
-            print("Something went wrong")
+    process_database()
 
 if __name__ == "__main__":
     main()
